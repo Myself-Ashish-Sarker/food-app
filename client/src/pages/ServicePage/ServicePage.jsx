cb5c71329d45630c7f7e80a2866abd7f7a492e8c
Ashish Sarker <myselfashishsarker@gmail.com>
2024-08-28 23:08:41 +0600

    react-router-dom tested and working

---
+++ client/src/pages/ServicePage/ServicePage.jsx
@@ -0,0 +1,9 @@
+const ServicePage = () => {
+    return (
+        <div>
+            <h1>This is Service Page</h1>
+        </div>
+    );
+};
+
+export default ServicePage
--- client/src/router/router.jsx
+++ client/src/router/router.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Root/Root";
+import HomePage from "../pages/HomePage/HomePage";
+import ServicePage from "../pages/ServicePage/ServicePage";
+import AboutPage from "../pages/AboutPage/AboutPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Root />
+        element: <Root />,
+        children: [
+            {
+                path: "/",
+                element: <HomePage />
+            },
+            {
+                path: "/service",
+                element: <ServicePage />
+            },
+            {
+                path: "/about",
+                element: <AboutPage />
+            }
+        ]
     },
 ]);
 
