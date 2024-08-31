abc36f63509ac55f1100cea63fe6ee002ec27620
Ashish Sarker <myselfashishsarker@gmail.com>
2024-08-31 19:36:47 +0600

    register page responsived

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
     return (
         <div>
             <div className="relative">
-                <img className="object-fill h-screen w-screen opacity-40" src={img1} alt="" />
+                <img className="object-cover h-screen w-screen opacity-40" src={img1} alt="" />
                 <div className="absolute top-5 right-5 z-30">
                     <Link to="/">
                         <button className="cursor-pointer px-4 py-2 rounded-md bg-slate-900 text-white items-center flex gap-2">
@@ -31,7 +31,7 @@ const Register = () => {
                 </div>
             </div>
             <div className="absolute inset-0 flex items-center justify-center">
-                <div className="flex justify-center gap-24">
+                <div className="flex flex-col lg:flex-row justify-center gap-24">
                     <div className="bg-white">
                         <div className="bg-base-100 w-[22rem] shadow-xl">
                             <h1 className="pt-8 text-2xl text-center font-semibold">Register</h1>
@@ -71,7 +71,7 @@ const Register = () => {
                             </form>
                         </div>
                     </div>
-                    <div className="flex flex-col justify-center gap-5">
+                    <div className="lg:flex flex-col justify-center gap-5 hidden lg:block">
                         <h1 className="text-4xl font-extrabold">Create Account</h1>
                         <h1 className="text-2xl font-semibold">What will you get?</h1>
 
